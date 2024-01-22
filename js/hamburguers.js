26ebf4ea18722aacc418af7f1f47c827efa42278
mi nombre <desarrollowebaps@gmail.com>
2024-01-21 21:58:52 -0400

    88_targeVeterinaria

--- js/hamburguers.js
+++ js/hamburguers.js
@@ -3,11 +3,13 @@
   
   
   // Look for .hamburger
+  let menu__nav = document.querySelector("#menu__nav")
   //var contEnlaces = document.getElementById("menuenlaces");
-  var hamburger = document.querySelector(".hamburger");
+  let hamburger = document.querySelector(".hamburger");
   // On click
   hamburger.addEventListener("click", function() {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
+    menu__nav.classList.toggle("menu__nav_JS")
     // Do something else, like open/close menu
   });
